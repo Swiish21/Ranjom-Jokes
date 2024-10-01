8d53fc88548d2748bb6cc25632931afb2c45aa2a
Malcom Otieno Adrian <154547110+Swiish21@users.noreply.github.com>
2024-10-01 08:33:54 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,14 @@
+import requests
+from django.shortcuts import render
+
+def index(request):
+  response = requests.get("https://dog.ceo/api/breeds/image/random")
+  data = response.json()
+  result = data["message"]
+  return render(request, 'templates/index.html', {'result': result})
+
+def randomJoke(request):
+  response1 = requests.get('https://official-joke-api.appspot.com/random_joke')
+  data2 = response1.json()
+  result1 = data2["message"]
+  return render(request, 'templates/index.html', {'result': result1})
